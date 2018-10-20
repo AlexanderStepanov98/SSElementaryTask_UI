f39ba07aba70a62e70c53e967dff2faa7e509598
AlexanderStepanov <alex.stdp@gmail.com>
2018-10-20 21:20:03 +0300

    task_02 completely rewritten + some css style modifications.

--- src/task_02/task_02.js
+++ src/task_02/task_02.js
@@ -1,82 +1,61 @@
-function envelopeCompare (firstEnvelope, secondEnvelope) {
+function envelopeCompare () {
     
-    // Initializing variables.
-    let a;
-    let b;
-    let c;
-    let d;
-    let counter = 0;
-    let errorMessage = "";
-    let inputDataIsCorrect;
+    // Initializing variables
+    var firstSide1 = Number($("#firstSide1").val());
+    var firstSide2 = Number($("#firstSide2").val());
+    var secondSide1 = Number($("#secondSide1").val());
+    var secondSide2 = Number($("#secondSide2").val());
     
-    // Setting the values of the first envelope.
-    Object.keys(firstEnvelope).forEach(function (key) {
-        if (counter == 0) {
-            a = firstEnvelope[key];
-        } else if (counter == 1) {
-            b = firstEnvelope[key];
-        } else if (counter > 1) {
-            return (errorMessage = "Error. Objects have incorrect number of fields. Objects must have only 2 fields.");
-        }
-        counter += 1;     
-    });
+    // Constructor for the envelopes
+    function Envelope (side1, side2) {
+        this.side1 = side1,
+        this.side2 = side2    
+    }
     
-    counter = 0;
-    
-    // Setting the values of the second envelope.
-    Object.keys(secondEnvelope).forEach(function (key) {
-        if (counter == 0) {
-            c = secondEnvelope[key];   
-        } else if (counter == 1) {
-            d = secondEnvelope[key];
-        } else if (counter > 1) { 
-            return (errorMessage = "Error. Objects have incorrect number of fields. Objects must have only 2 fields.");
-        }
-        counter += 1;     
-    });
-    
-    
-    // Checking if the input data is correct.
-    inputDataIsCorrect = !(isNaN(Number(a + b + c + d)));
-        
-    // Analyzing which envelope can fit in another one. 
-    if (inputDataIsCorrect) {
-        if ((a > c) && (b > d)) {
-            console.log("Second envelope can fit in first envelope");
-        } else if ((a < c) && (b < d)) {
-            console.log("First envelope can fit in second envelope");
-        } else if ((a > d) && (b > c)) {
-            console.log("Second envelope can fit in first envelope");
-        } else if ((a < d) && (b < c)) {
-            console.log("First envelope can fit in second envelope");
-        } else if (((a > c) && (b < d)) || ((a < c) && (b > d)))  {
-            if ((a > d) && (b > c)) {
-                console.log("Second envelope can fit in first envelope");
-            } else if ((a < d) && (b < c)) {
-                console.log("First envelope can fit in second envelope");
-            } else {
-                console.log("First envelope and second envelope do not match");
-            }
-        } else if (((a > d) && (b < c)) || ((a < d) && (b > c))) {
-            if ((a > c) && (b > d)) {
-                console.log("Second envelope can fit in first envelope");
-            } else if ((a < c) && (b < d)) {
-                console.log("First envelope can fit in second envelope");
-            } else {
-                console.log("First envelope and second envelope do not match");
-            }
+    // Input data validation
+    function validation () {
+        if (Number.isInteger(firstSide1) && Number.isInteger(firstSide2) && Number.isInteger(secondSide1) && Number.isInteger(secondSide2)) {
+            return true;
         } else {
-            console.log("First envelope and second envelope do not match");
+            $("#result").text("Error. Input data is incorrect.");
+            $("#result").css("color", "red");
+            return false;
         }
-    } else {
-        errorMessage = "Error. Objects must have only fields with numeric values.";
     }
     
-    // Finishing the function or outputing the error message. 
-    if (errorMessage) {
-        return errorMessage;
-    } else {
-        return "Envelope task completed successfully!";
+    // Creating envelope, where side 1 > side 2
+    function envelopeCreate (side1, side2) {
+        if (side1 >= side2) {
+            var envelope = new Envelope(side1, side2);
+        } else {
+            var envelope = new Envelope(side2, side1);
+        }
+        return envelope;
     }
     
+    // Comparing envelopes and showing the result
+    function compare () {
+        var isValid = validation();
+        if (isValid) {
+            var firstEnv = envelopeCreate(firstSide1, firstSide2);
+            var secondEnv = envelopeCreate(secondSide1, secondSide2);
+            if ((firstEnv.side1 > secondEnv.side1) && (firstEnv.side2 > secondEnv.side2)) {
+                $("#result").text("Second envelope can be fit in first envelope");
+            } else if ((secondEnv.side1 > firstEnv.side1) && (secondEnv.side2 > firstEnv.side2)) {
+                $("#result").text("First envelope can be fit in second envelope");
+            } else if ((firstEnv.side1 == secondEnv.side1) && (firstEnv.side2 == firstEnv.side2)) {
+                $("#result").text("Envelopes are equal");
+            } else {
+                $("#result").text("Any envelope can't be fit into another one");
+            }
+            $("#result").css("color", "#4CAF50");
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    return compare();
 }
+    
+$("#envelopeCompare").click(envelopeCompare);     
