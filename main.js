12ca0289315db99d960d896a1fb8e487cba93850
AlexanderStepanov <alex.stdp@gmail.com>
2018-10-21 15:27:00 +0300

    Script-pathes in the html files fixed. Added styles for task_03 buttons. Deleted comments from main.js file.

--- main.js
+++ main.js
@@ -4,29 +4,5 @@ window.onload = function() {
         $("#task_description").load("src/" + this.id + "/" + this.id + ".html");
     }
     
-    $(".taskCallBtn").click(loadTask);
-    
-    
-//    function includeTasks (arrayOfTasksUrl) {
-//        for (let i = 0; i <= arrayOfTasksUrl.length - 1; i++) {
-//            var script = document.createElement('script');
-//            script.src = arrayOfTasksUrl[i];
-//            document.getElementsByTagName('body')[0].appendChild(script);    
-//        }   
-//    }
-//    
-//    function createTaskDescription () {
-//        
-//        document.getElementById("task_description").innerHTML = taskDescription[this.id];
-//    }
-//    
-//    function createTaskBtnHandlers (taskDescriptionButtons) {
-//        for (let i = 0; i <= taskDescriptionButtons.length - 1; i++) {
-//            taskDescriptionButtons[i].onclick = createTaskDescription;
-//        }
-//    }
-//    
-//    includeTasks(arrayOfTasksUrl);
-//    createTaskBtnHandlers(taskDescriptionButtons);
-    
+    $(".taskCallBtn").click(loadTask);    
 }
