e1787ed04b4e7ff2f111ebfc97e6276f1bb84ef8
AlexanderStepanov <alex.stdp@gmail.com>
2018-10-21 19:39:24 +0300

    task_04.js completely rewritten.

--- src/task_04/task_04.js
+++ src/task_04/task_04.js
@@ -1,61 +1,64 @@
-function isPalindrome (number) {
+function isPalindrome () {
     
-    // Initializing variables.
-    let arrayOfNumber = [];
-    let tempArray = [];
-    let palindrome = 0;
-    let errorMessage = "";
+    // Initializing variables
+    var number = $("#number").val();
+    var palindrome = [];
+    var arrayOfPalindrome = [];
     
-    // Input data validation.
-    // создать объект ОШИБКА
-    // должна возвращать true или false
-    function validation (number) {
-        if (typeof(number) != typeof(0)) {
-            return "Error. Argument must be the type of number."
-        } else if (number <= 10) {
-            return "Error. Number must be more than 10."
-        } else if ((number + "").indexOf(".") > 0) {
-            return "Error. Number must be integer."
+    // Input data validation
+    function validation () {
+        if (Number.isInteger(Number(number)) && (Number(number) > 0)) {
+            return true;
         } else {
-            return "";
+            $("#result").text("Error. Input data is incorrect.");
+            $("#result").css("color", "#f44336");
+            return false;
         }
     }
     
-    // Finding the palindrome.
-    function findPalindrome (number) {
-        let j = 0;
-        let a = 0;
-        let b = 0;
-        arrayOfNumber = number.toString().split('');
-        while (j <= arrayOfNumber.length - 2) {
-            if (Number(arrayOfNumber[j]) == Number(arrayOfNumber[j+1])) {
-                a = j;
-                b = j+1;
-                while (Number(arrayOfNumber[a]) == Number(arrayOfNumber[b])) {
-                    tempArray.unshift(arrayOfNumber[a]);
-                    tempArray.push(arrayOfNumber[b]);
-                    console.log("arrayOfNumber[a] = ", arrayOfNumber[a]);
-                    console.log("arrayOfNumber[b] = ", arrayOfNumber[b]);
-                    a -= 1;
-                    b += 1;
+    // if the entered number is even
+    function findPalindrome (token) {
+        var isValid = validation();
+        if (isValid) {
+            var i = 0;
+            var left;
+            var right;
+            while (i < number.length - token) {
+                if (number[i] == number[i + token]) {
+                    left = i;
+                    right = i + token;
+                    if (token == 2) { palindrome.push(number[i+1]) };
+                    while ((number[left] == number[right]) && (left != -1) && (right != number.length)) {
+                        palindrome.push(number[right]);
+                        palindrome.unshift(number[left]);
+                        left--;
+                        right++;
+                    }
+                    arrayOfPalindrome.push(palindrome.join(""));
+                    palindrome = [];
                 }
+                i++;
             }
-            j += 1;
-            if (tempArray.length > 1) {
-                j = arrayOfNumber.length;
-            }
+            palindromeShow();
+            return arrayOfPalindrome;
+        } else {
+            return false;
         }
-        number = tempArray.join('');
-        return Number(number);
     }
     
-    // Main programm.
-    errorMessage = validation(number);
-    
-    if (errorMessage) {
-        return errorMessage;
-    } else {
-        return palindrome = findPalindrome(number);
+    // Showing palindrome in the web-page
+    function palindromeShow () {
+        if (arrayOfPalindrome.length > 0) {
+            $("#result").text(arrayOfPalindrome.join(", "));
+            $("#result").css("color", "#4CAF50");
+        } else {
+            $("#result").text("Number is not a palindrome.");
+            $("#result").css("color", "#f44336");
+        }      
     }
-    
+
+    findPalindrome(1); // Find all even palindromes
+    findPalindrome(2); // Find all odd palindromes
 }
+
+$("#isPalindrome").click(isPalindrome);
