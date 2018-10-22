1191459eee9514602d368ce7e8ec0a60b3678ff3
AlexanderStepanov <alex.stdp@gmail.com>
2018-10-22 19:24:56 +0300

    task_05.js completely rewritten.

--- src/task_05/task_05.js
+++ src/task_05/task_05.js
@@ -1,82 +1,102 @@
-function luckyTickets(context) {
+function luckyTickets() {
     
-    let minStr;
-    let maxStr;
-    let winnerAlgorythm = {
-        winnerAlgorythmName : "",
+    // Initializing variables
+    var minTicket = Number($("#minTicket").val());
+    var maxTicket = Number($("#maxTicket").val());
+    var winnerAlgorythm = {
+        Name : "",
         numberOfEasyTickes : 0,
         numberOfHardTickes : 0
     };
     
-    function isValid(context) {
-        if (!context) {
-            return false;
-        } else if (typeof(context) != typeof({})) {
-            return false;
-        } else if (context.min == undefined || context.max == undefined || isNaN(Number(context.min)) || isNaN(Number(context.max)) || typeof(context.min) == typeof("") || typeof(context.max) == typeof("")) {
-            return false;
-        } else if ((context.min < 0) || (context.max > 999999)) {
-            return false;
-        } else if (context.min >= context.max) {
-            return false;
-        } else {
-            context.min = String(context.min);
-            context.max = String(context.max);
-            for (let i = 0; i < 6; i++) {
-                if ((context.min).length < 6) {
-                    context.min = "0" + context.min;
-                }
-                if ((context.max).length < 6) {
-                    context.max = "0" + context.max;
-                }
-            }
+    // Tickets constructor
+    function Tickets (min, max) {
+        this.min = min,
+        this.max = max    
+    }
+    
+    // Input data validation
+    function validation () {
+        if (Number.isInteger(minTicket) && Number.isInteger(maxTicket) && (minTicket >= 0) && (minTicket < 999999) && (maxTicket <= 999999) && (maxTicket > 0) && (maxTicket > minTicket)) {
             return true;
-        }
-    }
-    
-    function minValueIncrement(context) {
-        context.min = Number(context.min) + 1;
-        context.min = String(context.min);
-        for (let i = 0; i < 6; i++) {
-            if ((context.min).length < 6) {
-                context.min = "0" + context.min;
-            }
-        }
-    }
-    
-    function countLuckyTickets(context) {
-        let easyAlgorythmLeftSum;
-        let easyAlgorythmRightSum;
-        let hardAlgorythmEvenSum;
-        let hardAlgorythmOddSum;
-        for (let i = Number(context.min); i < Number(context.max); i++) {
-            easyAlgorythmLeftSum = Number(context.min[0]) + Number(context.min[1]) + Number(context.min[2]);
-            easyAlgorythmRightSum = Number(context.min[3]) + Number(context.min[4]) + Number(context.min[5]);
-            hardAlgorythmEvenSum = Number(context.min[5]) + Number(context.min[3]) + Number(context.min[1]);
-            hardAlgorythmOddSum = Number(context.min[4]) + Number(context.min[2]) + Number(context.min[0]);
-            if (easyAlgorythmLeftSum == easyAlgorythmRightSum) {
-                winnerAlgorythm.numberOfEasyTickes += 1;
-            }
-            if (hardAlgorythmEvenSum == hardAlgorythmOddSum) {
-                winnerAlgorythm.numberOfHardTickes += 1;
-            }
-            minValueIncrement(context);
-        }
-        if (winnerAlgorythm.numberOfEasyTickes > winnerAlgorythm.numberOfHardTickes) {
-            winnerAlgorythm.winnerAlgorythmName = "Easy algorythm have won!";
-        } else if (winnerAlgorythm.numberOfEasyTickes < winnerAlgorythm.numberOfHardTickes){
-            winnerAlgorythm.winnerAlgorythmName = "Hard algorythm have won!";
         } else {
-            winnerAlgorythm.winnerAlgorythmName = "It's a draw!";
+            $("#result").text("Error. Input data is incorrect.");
+            $("#result").css("color", "#f44336");
+            return false;
         }
-        return winnerAlgorythm;
     }
     
-    
-    if (isValid(context)) {
-        return countLuckyTickets(context); 
-    } else {
-        return "Mistake";
+    // Converting numbers into 6-digit value
+    function numbersCorrection () {
+        minTicket = String(minTicket);
+        maxTicket = String(maxTicket);
+        for (let i = 0; i < 6; i++) {
+            if (minTicket.length < 6) {
+                minTicket = "0" + minTicket;
+            }
+            if (maxTicket.length < 6) {
+                maxTicket = "0" + maxTicket;
+            }
+        }
     }
     
+    // Counting lucky tickets
+    function countLuckyTickets () {
+        
+        var isValid = validation();
+        var leftSum;
+        var rightSum;
+        var evenSum;
+        var oddSum;
+        
+        // Incrementing the min value
+        function minValueIncrement(tickets) {
+            tickets.min = Number(tickets.min) + 1;
+            tickets.min = String(tickets.min);
+            for (let i = 0; i < 6; i++) {
+                if ((tickets.min).length < 6) {
+                    tickets.min = "0" + tickets.min;
+                }
+            }
+        }
+        
+        // Showing the result in the web-page
+        function showResult () {
+            $("#result").html(winnerAlgorythm.Name + "<br>Number of easy tickets: " + winnerAlgorythm.numberOfEasyTickes + "<br> Number of hard tickets: " + winnerAlgorythm.numberOfHardTickes);
+            $("#result").css("color", "#4CAF50");
+        }
+        
+        if (isValid) {
+            numbersCorrection();
+            var tickets = new Tickets (minTicket, maxTicket);
+            for (var i = Number(tickets.min); i < Number(tickets.max); i++) {
+                leftSum = Number(tickets.min[0]) + Number(tickets.min[1]) + Number(tickets.min[2]);
+                rightSum = Number(tickets.min[3]) + Number(tickets.min[4]) + Number(tickets.min[5]);
+                evenSum = Number(tickets.min[5]) + Number(tickets.min[3]) + Number(tickets.min[1]);
+                oddSum = Number(tickets.min[4]) + Number(tickets.min[2]) + Number(tickets.min[0]);
+                if (leftSum == rightSum) {
+                    winnerAlgorythm.numberOfEasyTickes += 1;
+                };
+                if (evenSum == oddSum) {
+                    winnerAlgorythm.numberOfHardTickes += 1;
+                };
+                minValueIncrement(tickets);
+            }
+            if (winnerAlgorythm.numberOfEasyTickes > winnerAlgorythm.numberOfHardTickes) {
+                winnerAlgorythm.Name = "Easy algorythm have won!";
+            } else if (winnerAlgorythm.numberOfEasyTickes < winnerAlgorythm.numberOfHardTickes){
+                winnerAlgorythm.Name = "Hard algorythm have won!";
+            } else {
+                winnerAlgorythm.Name = "It's a draw!";
+            }
+            showResult();
+            return winnerAlgorythm;
+        } else {
+            return false;
+        }
+    }    
+    
+    return countLuckyTickets();  
 }
+
+$("#luckyTickets").click(luckyTickets);
